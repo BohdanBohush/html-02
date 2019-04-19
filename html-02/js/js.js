7cea9b101f8d35e1248a256fc3a3e743609ba17b
Bohdan Bohush <41343084+BohdanBohush@users.noreply.github.com>
2019-04-19 16:41:22 +0300

    Add files via upload

---
+++ html-02/js/js.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+/*	$("img").click(function(){
+		var obj=$(this);
+		ibj.animate({height:'400px',opacity:'0.2'},'slow');
+		
+	})*/
+	
+	$("h1").animate({fontSize:'100px'},2000);
+});
